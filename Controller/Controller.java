9f361e1b003b6760235d71cc5f6aa89a05b6be0d
dabin cho <daroo2473@naver.com>
2021-11-12 06:13:32 +0900

    filter add, delete/ 필터 관련 수정

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -6,13 +6,24 @@ import java.awt.event.ActionListener;
 //Class for test controller
 public class Controller {
 	//variable
-	private NationDataModel data_model;
-	private SearchFilterModel filter_moder;
-	private View view_client;
+	static private NationDataModel data_model;
+	static private SearchFilterModel filter_model;
+	static private View view_client;
 	
 	//run project
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		data_model = new NationDataModel();
+		filter_model = new SearchFilterModel();
+        view_client = new View();
+        
+        view_client.setSearchView();
+        //view_client.run();
+        
+        //test code
+        Controller controll = new Controller();
+        controll.addFilter("기후", "?", "온난");
+        controll.deleteFilter("기후", "?", "온난");
+        System.out.print("성공\n");
 	}
 	
 	//find data
@@ -21,10 +32,16 @@ public class Controller {
 	
 	//add filter
 	public void addFilter(String type, String oper, String text) {
+		SearchFilter filter = new SearchFilter(type, oper, text);
+		filter_model.add(filter);
+		
+		//System.out.print(filter_model.getFilter(0).getType() + "\n");
 	}
 	
 	//delete filter
-	public void deleteFilter(String type, String oper, String Text) {
+	public void deleteFilter(String type, String oper, String text) {
+		SearchFilter filter = new SearchFilter(type, oper, text);
+		filter_model.delete(filter);
 	}
 	
 	//set event Listener - ?
--- Controller/SearchFilterModel.java
+++ Controller/SearchFilterModel.java
@@ -3,10 +3,53 @@ package Controller;
 import java.util.*;
 
 public class SearchFilterModel{
-	private List<SearchFilter> filter_list;
+	//private List<SearchFilter> filter_list;
+	private ArrayList<SearchFilter> filter_list;
+	private int filter_num;
+	
+	public SearchFilterModel() {
+		filter_list = new ArrayList<SearchFilter>();
+		filter_num = 0;
+	}
 	
 	//temporary method
 	public String getSql() {
 		return "Sql Data";
+	}
+	
+	//get filter(temporary Method)
+	public List<SearchFilter> getFilter() {
+		return filter_list;
+	}
+	public SearchFilter getFilter(int index) {
+		return filter_list.get(index);
+	}
+	
+	//add
+	public void add(SearchFilter sf) {
+		String type;
+		for(int i = 0; i<filter_num; i++) {
+			type = filter_list.get(i).getType();
+			if(type.equals(sf.getType()))
+					return;
+		}
+		filter_list.add(sf);
+		filter_num++;
+		System.out.print(filter_num + "\n");
+	}
+	
+	//delete
+	public void delete(SearchFilter sf) {
+		String type;
+		for(int i = 0; i<filter_num; i++) {
+			type = filter_list.get(i).getType();
+			if(type.equals(sf.getType())) {
+				filter_list.remove(i);
+				filter_num--;
+				
+				System.out.print(filter_num + "\n");
+				return;
+			}
+		}
 	}
 }
