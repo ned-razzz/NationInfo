7eb43b08c844a0454d98c438f016e66cd4b6cb65
SleepDong <89907406+SleepDong@users.noreply.github.com>
2021-11-12 15:34:13 +0900

    dd

---
+++ error/src/error/errors.java
@@ -0,0 +1,12 @@
+package error;
+
+public class errors {
+
+	public static void main(String[] args) {
+		System.out.println("이 프로그램은 많은 오류들을 가지고 있다.");
+		System.out.println("그러나 프로그램이 이 문장을 출력한다면");
+		System.out.println("당신은 모든 오류들을 고친 것이다.");
+
+	}
+
+}
