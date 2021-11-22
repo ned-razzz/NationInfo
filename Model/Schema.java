1d6977f3de3fcc2d1acf63b1c9f1be3a20b7f769
Ned raZZZ <smg9972@gmail.com>
2021-11-23 01:25:43 +0900

    데이터베이스 타입과 관련된 enum 클래스 생성. 데이터베이스 타입들은 상수화시켜서 타입 실수를 줄이고 유지보수에 유리해짐.

---
+++ Model/Schema.java
@@ -0,0 +1,29 @@
+package Model;
+
+public enum Schema {
+    NAME("contry"),
+    CODE("contry_code"),
+    CAPITAL("capital"),
+    CLIMATE("climate"),
+    LOC("location"),
+    CITIES("major_cities"),
+    RELIGION("religion"),
+    NATION("nation"),
+    MEDIA("media"),
+    SIZE("area"),
+    SOURCE("area_source"),
+    SIZE_COMP("area_inst"),
+    LANG("lang");
+
+
+    private final String type;
+
+    private Schema(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
